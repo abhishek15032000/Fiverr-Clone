bd6b0727f52959b9a32ddbbdd7af0ba0fc5e2ff2
Abhishek <defabhishekkumarsingh@gmail.com>
2023-04-29 05:14:24 +0530

    carousel component completed , third-commit

--- pages/home/home.jsx
+++ pages/home/home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import Featured from '../../components/Featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
+import SliderComp from '../../components/Slider/Slider';
+import CategoryCard from '../../components/categoryCard/CategoryCard';
+import { cards } from '../../src/data';
+
 const Home=()=>{
   return (
     <div className='home'>
       <Featured/>
+      <TrustedBy/>
+      <SliderComp slidesToShow={5} arrowsScroll={5}>
+         {
+            cards.map(card=>(
+              <CategoryCard key={card.id} item={card}/>
+            ))
+         }
+      </SliderComp>
     </div>
   )
 }
