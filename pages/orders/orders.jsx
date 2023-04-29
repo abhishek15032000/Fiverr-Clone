59ed5caf4841ac0422af88e81715498171f8d8e1
Abhishek <defabhishekkumarsingh@gmail.com>
2023-04-29 20:06:00 +0530

    my Gigs , my Orders page done --eighth commit

--- pages/orders/orders.jsx
+++ pages/orders/orders.jsx
@@ -1,8 +1,132 @@
 import React from 'react'
 
 function Orders() {
+   
+  const currentUser={
+      id:1,
+      username:'John Doe',
+      isSeller:true
+  }
+
   return (
-    <div>Orders</div>
+    <div className="Orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller?"Buyer":"Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Stunning concept art</td>
+            <td>
+              59.99
+            </td>
+            <td>13</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>
+              120.99
+            </td>
+            <td>41</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>
+              79.99
+            </td>
+            <td>55</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>
+              119.99
+            </td>
+            <td>29</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>
+              59.99
+            </td>
+            <td>34</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>
+              110.99
+            </td>
+            <td>16</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
